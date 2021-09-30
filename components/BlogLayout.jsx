d5cb3b0b599022b6857ac9f66c0239d8661f5a86
Jamal Gardiner <jamalgardiner7@live.com>
2021-09-30 12:35:37 -0400

    Feat: nextSEO on all the blog posts. blog posts updated to have their url

--- components/BlogLayout.jsx
+++ components/BlogLayout.jsx
@@ -5,6 +5,7 @@ import Newsletter from './Newsletter'
 import Banner from './Banner'
 import RecentTen from './RecentTen'
 import Footer from './Footer'
+import { NextSeo } from 'next-seo'
 
 //todo: breadcrumbs
 export default function BlogLayout({ meta, children }) {
@@ -13,11 +14,37 @@ export default function BlogLayout({ meta, children }) {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta charSet="utf-8" />
-                <meta name="Description" content={meta.description}></meta>
-                <title>{meta.title + ' - The Life Of Jamal'}</title>
+                {/* Open Graph Protocol */}
             </Head>
             <main>
                 <div sx={{ height: `calc(100vh - 120px)` }}>
+                    {console.log(meta)}
+                    <NextSeo
+                        title={meta.title}
+                        description={meta.description}
+                        author="Jamal Gardiner"
+                        openGraph={{
+                            site_name: 'thelifeofjamal',
+                            title: meta.title,
+                            description: meta.description,
+                            images: [{ url: meta.image }],
+                            type: 'website',
+                            url: meta.url,
+                            profile: { firstName: 'Jamal', lastName: 'Gardiner', gender: 'Male' },
+                            article: {
+                                publishedTime: new Date(meta.date),
+                                authors: ['Jamal Gardiner']
+                            }
+                        }}
+                        twitter={{
+                            handle: '@GardinerJamal',
+                            site: 'thelifeofjamal',
+                            title: meta.title,
+                            cardType: 'summary_large_image',
+                            image: meta.image,
+                            description: meta.description
+                        }}
+                    />
                     <Banner />
                     <div sx={{ maxWidth: '70%', ml: 'auto', mr: 'auto', pr: '5' }}>
                         <Grid gap={2} columns={[1, null, 2, '2.5fr 1fr']}>
