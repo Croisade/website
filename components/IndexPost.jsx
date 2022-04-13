c7d6d1f770e55d23f0a6678184b6f57727b2d310
Jamal Gardiner <jamalgardiner7@live.com>
2022-04-13 17:49:41 -0400

    feat: getStatic props in each index.js using gray matter and fs & glob. Blog posts aren't working.

--- components/IndexPost.jsx
+++ components/IndexPost.jsx
@@ -3,17 +3,13 @@ import { jsx } from 'theme-ui'
 import Link from 'next/link'
 import { HeadPost } from './HeadPost'
 
-export const IndexPost = ({ post }) => {
-    const {
-        link,
-        module: { meta }
-    } = post
-
+export const IndexPost = ({ data }) => {
+    console.log(data)
     return (
         <div>
             <article>
-                <HeadPost meta={meta} />
-                <Link href={'/blog' + link}>
+                <HeadPost data={data} />
+                <Link href={'/blog' + data.root + data.url}>
                     <a sx={{ color: 'primary', cursor: 'pointer' }}>Read more... </a>
                 </Link>
             </article>
