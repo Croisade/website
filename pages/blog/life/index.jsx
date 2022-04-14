30583526bbea820372bdc87ad964156a49ae99d4
Jamal Gardiner <jamalgardiner7@live.com>
2022-04-14 12:51:23 -0400

    feat: routes dynamically generated, images working

--- pages/blog/life/index.jsx
+++ pages/blog/life/index.jsx
@@ -6,17 +6,50 @@ import { Grid } from 'theme-ui'
 import Footer from '../../../components/Footer'
 import Banner from '../../../components/Banner'
 
-export default function IndexPage() {
+import path from 'path'
+import glob from 'glob'
+import matter from 'gray-matter'
+import sortBy from 'lodash/sortBy'
+import slice from 'lodash/slice'
+import reverse from 'lodash/reverse'
+import fs from 'fs'
+
+export default function IndexPage({posts}) {
     return (
         <div sx={{ maxWidth: '70%', ml: 'auto', mr: 'auto' }}>
             <Banner />
             <h1>Life</h1>
             <Grid gap={3} columns={[1, null, 2]}>
                 {posts.map((post) => (
-                    <IndexPost key={post.link} post={post} />
+                    <IndexPost key={post.link} data={post} />
                 ))}
             </Grid>
             <Footer />
         </div>
     )
+}
+
+export function getStaticProps() {
+    const postsPath = path.join(process.cwd(), 'posts/life')
+    const options = {
+        cwd: postsPath
+    }
+    const globbedPosts = glob.sync('**/*.mdx', options)
+
+    function grabFiles(paths) {
+        return fs.readFileSync(path.join(postsPath, paths), 'utf-8')
+    }
+    const files = globbedPosts.map(grabFiles)
+    const metas = files.map((x) => {
+        const { data } = matter(x)
+        return data
+    })
+
+    const sorted = sortBy(metas, function (x) {
+        const { date } = x
+        return new Date(date)
+    })
+
+    const posts = reverse(sorted)
+    return { props: { posts } }
 }
