f8fca532c453d14aa16a31736894d95cbc049ad6
Jamal Gardiner <jamalgardiner7@live.com>
2022-04-18 15:46:25 -0400

    feat: styling is consistent with productions

--- components/RecentTen.jsx
+++ components/RecentTen.jsx
@@ -1,12 +1,14 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import SidePost from './SidePost'
-import { Grid } from 'theme-ui'
+import { Grid, Heading } from 'theme-ui'
 
 export default function RecentTen({ data }) {
     return (
         <div sx={{ maxWidth: '100%', ml: 'auto', mr: 'auto', textAlign: 'center' }}>
-            <h1>Recent Posts</h1>
+            <Heading as="h2" sx={{ mt: '3', mb: '3' }}>
+                Recent Posts
+            </Heading>
             <Grid gap={3} columns={[1, null, 1]}>
                 {data.map((sortedPosts) => (
                     <SidePost key={sortedPosts.url} post={sortedPosts} />
