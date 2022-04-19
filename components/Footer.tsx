2d98bbe98447a071349cadaae093ef33c2dc214b
Jamal Gardiner <jamalgardiner7@live.com>
2022-04-19 12:00:50 -0400

    feat: replace hardcoded strings with constants in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource theme-ui */
-import { WEBSITE_INFORMATION } from '@constants'
+import { WEBSITE_INFORMATION, FOOTER_LINKS } from '@constants'
 import Link from 'next/link'
 import { Box, Flex, Grid, Heading, Text } from 'theme-ui'
 
@@ -18,146 +18,30 @@ const Footer = () => {
                         px: 2,
                         py: 4
                     }}>
-                    <Grid columns={[2, null, 4, '1fr 1fr 1fr 1fr']}>
+                    <Grid columns={[1, null, 2, '3fr 1fr']}>
                         <Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <Link href="/">
-                                        <a
-                                            sx={{
-                                                variant: 'styles.navlink',
-                                                p: 2,
-                                                color: 'styles.text',
-                                                justifyContent: 'center'
-                                            }}>
-                                            Home
-                                        </a>
-                                    </Link>
-                                </Flex>
-                            </Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <Link href="/blog">
-                                        <a
-                                            sx={{
-                                                variant: 'styles.navlink',
-                                                p: 2,
-                                                color: 'styles.text'
-                                            }}>
-                                            Blog
-                                        </a>
-                                    </Link>
-                                </Flex>
-                            </Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <Link href="/about">
-                                        <a
-                                            sx={{
-                                                variant: 'styles.navlink',
-                                                p: 2,
-                                                color: 'styles.text'
-                                            }}>
-                                            About
-                                        </a>
-                                    </Link>
-                                </Flex>
-                            </Box>
+                            <Grid columns={[2, null, 3, '1fr 1fr 1fr']}>
+                                {FOOTER_LINKS.map((x) => {
+                                    return (
+                                        <Box key={x.url} sx={{ display: 'block' }}>
+                                            <Flex sx={{ justifyContent: 'center' }}>
+                                                <Link href={x.url}>
+                                                    <a
+                                                        sx={{
+                                                            variant: 'styles.navlink',
+                                                            p: 2,
+                                                            color: 'styles.text',
+                                                            justifyContent: 'center'
+                                                        }}>
+                                                        {x.siteName}
+                                                    </a>
+                                                </Link>
+                                            </Flex>
+                                        </Box>
+                                    )
+                                })}
+                            </Grid>
                         </Box>
-                        <Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <a
-                                        href="https://www.youtube.com/channel/UCr8h9u258fq605akq_dGA1A?sub_confirmation=1"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        sx={{
-                                            variant: 'styles.nav',
-                                            p: 2,
-                                            color: 'text'
-                                        }}>
-                                        Youtube
-                                    </a>
-                                </Flex>
-                            </Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <a
-                                        href="https://www.facebook.com/YTJamalGardiner"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        sx={{
-                                            variant: 'styles.nav',
-                                            p: 2,
-                                            color: 'text'
-                                        }}>
-                                        Facebook
-                                    </a>
-                                </Flex>
-                            </Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <a
-                                        href="https://www.instagram.com/the_life_of_jamal_/"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        sx={{
-                                            variant: 'styles.nav',
-                                            p: 2,
-                                            color: 'text'
-                                        }}>
-                                        Instagram
-                                    </a>
-                                </Flex>
-                            </Box>
-                        </Box>
-                        <Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <a
-                                        href="https://github.com/Croisade"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        sx={{
-                                            variant: 'styles.nav',
-                                            p: 2,
-                                            color: 'text'
-                                        }}>
-                                        Github
-                                    </a>
-                                </Flex>
-                            </Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <a
-                                        href="https://www.amazon.com/s?rh=n%3A7141123011%2Cp_4%3ATLOJ&ref=bl_sl_s_ap_web_7141123011"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        sx={{
-                                            variant: 'styles.nav',
-                                            p: 2,
-                                            color: 'text'
-                                        }}>
-                                        Merchandise
-                                    </a>
-                                </Flex>
-                            </Box>
-                            <Box>
-                                <Flex sx={{ justifyContent: 'center' }}>
-                                    <Link href="/peripherals">
-                                        <a
-                                            sx={{
-                                                variant: 'styles.navlink',
-                                                p: 2,
-                                                color: 'text'
-                                            }}>
-                                            Peripherals
-                                        </a>
-                                    </Link>
-                                </Flex>
-                            </Box>
-                        </Box>
-
                         <Box>
                             <Heading as="h4">Disclosure</Heading>
                             <Text>{WEBSITE_INFORMATION.amazonAssociateDisclaimer}</Text>
