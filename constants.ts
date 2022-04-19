8acac04afaacfc2361f3084003a95c6c5c443201
Jamal Gardiner <jamalgardiner7@live.com>
2022-04-18 20:24:29 -0400

    feat: constants before babel deletion

---
+++ constants.ts
@@ -0,0 +1,23 @@
+export const PERSONAL_INFORMATION = {
+    firstName: 'Jamal',
+    lastName: 'Gardiner',
+    about:
+        'Developer, content creator and a learner of all things. Welcome to my website where I keep all of my thoughts and opinions. Check back every once in a while to witness one mans quest to over engineer a website.',
+    learnAboutMe: 'Learn more about me',
+    personalCaption: 'picture of Jamal',
+    handle: '@GardinerJamal',
+    gender: 'Male'
+}
+
+export const WEBSITE_INFORMATION = {
+    siteNameLowerCase: 'thelifeofjamal',
+    siteName: 'The Life Of Jamal',
+    home: 'Home',
+    type: 'website',
+    url: 'https://www.thelifeofjamal.com'
+}
+
+export const IMAGES = {
+    personalLogo: 'https://cdn1.thelifeofjamal.com/public/JamalLogo.png',
+    banner: 'https://cdn1.thelifeofjamal.com/banner.jpg'
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@
 import Banner from '@components/Banner'
 import Footer from '@components/Footer'
 import RecentThree from '@components/RecentThree'
+import { SEO } from '@components/SEO'
+import { IMAGES, PERSONAL_INFORMATION, WEBSITE_INFORMATION } from '@constants'
 import fs from 'fs'
 import glob from 'glob'
 import matter from 'gray-matter'
@@ -19,12 +21,12 @@ export default function Home({ posts }) {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta charSet="utf-8" />
-                <meta
-                    name="Description"
-                    content={
-                        'I am a developer, content creator and a learner of all things. Welcome to my website where I keep all of my thoughts and opinions. Everything you want to know and more, is here.'
-                    }></meta>
-                <title>{'The Life Of Jamal'}</title>
+                <SEO
+                    title={WEBSITE_INFORMATION.siteNameLowerCase}
+                    description={PERSONAL_INFORMATION.about}
+                    image={IMAGES.banner}
+                    url={WEBSITE_INFORMATION.url}
+                />
             </Head>
             <Banner />
             <div
@@ -34,28 +36,23 @@ export default function Home({ posts }) {
                     ml: 'auto',
                     mr: 'auto'
                 }}>
-                <Heading as="h1">Home</Heading>
-
+                <Heading as="h1">{WEBSITE_INFORMATION.home}</Heading>
                 <Grid gap={2} columns={[1, null, 2, '2.5fr 1fr']}>
                     <Box sx={{ mt: '3' }}>
                         <Heading as="h2" sx={{ mt: '4' }}>
-                            Jamal Gardiner
+                            {PERSONAL_INFORMATION.firstName + ' ' + PERSONAL_INFORMATION.lastName}
                         </Heading>
-                        <Text sx={{ mt: '2', display: 'block' }}>
-                            Developer, content creator and a learner of all things. Welcome to my
-                            website where I keep all of my thoughts and opinions. Check back every
-                            once in a while to witness one mans quest to over engineer a website.
-                        </Text>
+                        <Text sx={{ mt: '2', display: 'block' }}>{PERSONAL_INFORMATION.about}</Text>
                         <Link href="/about">
                             <Button variant="secondary" sx={{ mt: '5' }}>
-                                Learn more about me
+                                {PERSONAL_INFORMATION.learnAboutMe}
                             </Button>
                         </Link>
                     </Box>
                     <Box>
                         <Image
-                            src="https://cdn1.thelifeofjamal.com/public/JamalLogo.png"
-                            alt="Picture of Jamal"
+                            src={IMAGES.personalLogo}
+                            alt={PERSONAL_INFORMATION.personalCaption}
                             width={225}
                             height={300}
                             priority
