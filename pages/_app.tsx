7845d5d8445ed4b262405ea495cb84df7c978881
Jamal Gardiner <jamalgardiner7@live.com>
2022-06-10 12:00:02 -0400

    Intergrate GTM #14 feat: GTM integegration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource theme-ui */
 import Header from '@components/Header'
 import { ThemeProvider } from 'theme-ui'
+import Script from 'next/script'
 import '../styles/style.css'
 import theme from '../theme'
 
@@ -8,6 +9,13 @@ export default function App({ Component, pageProps }) {
     return (
         <ThemeProvider theme={theme}>
             <div>
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T2LCGFB')`}
+                </Script>
                 <Header />
                 <Component {...pageProps} />
             </div>
---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2LCGFB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+                    }}
+                />
+            </body>
+        </Html>
+    )
+}
